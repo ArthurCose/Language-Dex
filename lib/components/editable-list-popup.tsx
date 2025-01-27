78c0003670609d1876d212583c5a3af5cbd244bf
Arthur Cosentino <arthurcose@gmail.com>
2025-01-27 18:51:17 -0500

    Slightly increase row size is editable list to prevent squashing icon buttons

--- lib/components/editable-list-popup.tsx
+++ lib/components/editable-list-popup.tsx
@@ -379,7 +379,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    height: 44,
+    height: 48,
   },
   plainItem: {
     paddingLeft: 16,
