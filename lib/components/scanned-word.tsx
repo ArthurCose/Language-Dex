1f97e9123b586fe79252d2b5d0762f84778fe465
Arthur Cosentino <arthurcose@gmail.com>
2025-02-06 03:14:48 -0500

    More double render fixes

--- lib/components/scanned-word.tsx
+++ lib/components/scanned-word.tsx
@@ -1,22 +1,149 @@
 import React, { useMemo, useRef, useState } from "react";
-import { Text, StyleSheet, Pressable, View } from "react-native";
+import { StyleSheet, Pressable, View } from "react-native";
 import * as DropDownPrimitive from "@rn-primitives/dropdown-menu";
 import { useTheme } from "@/lib/contexts/theme";
 import useWordDefinitions from "@/lib/hooks/use-word-definitions";
 import { Span } from "./text";
 import { useTranslation } from "react-i18next";
 import { useUserDataContext } from "@/lib/contexts/user-data";
-import { namePartOfSpeech } from "@/lib/data";
+import { namePartOfSpeech, WordDefinitionData } from "@/lib/data";
 import * as Clipboard from "expo-clipboard";
 import { router } from "expo-router";
 import { logError } from "../log";
 
 type Props = { dictionaryId: number; text: string; lowercase: string };
 
+type DefinitionsBubbleProps = {
+  text: string;
+  lowercase: string;
+  definitionResult?: {
+    spelling: string;
+    definitions: WordDefinitionData[];
+  };
+  close: () => void;
+};
+
+function DefinitionsBubble({
+  text,
+  lowercase,
+  definitionResult,
+  close,
+}: DefinitionsBubbleProps) {
+  const theme = useTheme();
+  const [t] = useTranslation();
+  const [userData] = useUserDataContext();
+  const dictionary = userData.dictionaries.find(
+    (d) => d.id == userData.activeDictionary
+  );
+
+  return (
+    <DropDownPrimitive.Portal>
+      <DropDownPrimitive.Overlay
+        style={StyleSheet.absoluteFill}
+        onPress={close}
+      >
+        <DropDownPrimitive.Content align="center">
+          <View
+            style={[
+              styles.popup,
+              theme.styles.dialog,
+              theme.styles.definitionBubble,
+            ]}
+          >
+            {definitionResult && (
+              <>
+                <Pressable
+                  style={[styles.bordered, theme.styles.definitionBorders]}
+                  android_ripple={theme.ripples.popup}
+                  onPress={() => {
+                    router.navigate(`/words/${encodeURIComponent(lowercase)}`);
+                    close();
+                  }}
+                >
+                  <Span style={[styles.wordTitle]}>
+                    {definitionResult.spelling}
+                  </Span>
+                </Pressable>
+
+                {definitionResult.definitions.map((definition) => {
+                  const partOfSpeech = namePartOfSpeech(
+                    dictionary,
+                    definition.partOfSpeech
+                  );
+
+                  return (
+                    <Pressable
+                      key={definition.id}
+                      style={[
+                        styles.definitionBlock,
+                        styles.bordered,
+                        theme.styles.definitionBorders,
+                      ]}
+                      android_ripple={theme.ripples.popup}
+                      onPress={() => {
+                        router.navigate(
+                          `/words/${encodeURIComponent(
+                            lowercase
+                          )}/definition/${encodeURIComponent(definition.id)}`
+                        );
+                        close();
+                      }}
+                    >
+                      <Span style={[theme.styles.partOfSpeech]}>
+                        {partOfSpeech ?? t("unknown")}
+                      </Span>
+
+                      <Span style={[styles.definition]}>
+                        {definition.definition}
+                      </Span>
+
+                      {definition.example.length > 0 && (
+                        <Span style={[styles.definition, theme.styles.example]}>
+                          {definition.example}
+                        </Span>
+                      )}
+                    </Pressable>
+                  );
+                })}
+              </>
+            )}
+
+            <Pressable
+              style={[
+                styles.action,
+                styles.bordered,
+                theme.styles.definitionBorders,
+              ]}
+              android_ripple={theme.ripples.popup}
+              onPress={() => {
+                router.navigate(
+                  `/words/${encodeURIComponent(lowercase)}/definition/add`
+                );
+                close();
+              }}
+            >
+              <Span>{t("Add_Definition")}</Span>
+            </Pressable>
+
+            <Pressable
+              style={styles.action}
+              android_ripple={theme.ripples.popup}
+              onPress={() => {
+                Clipboard.setStringAsync(text).catch(logError);
+                close();
+              }}
+            >
+              <Span>{t("Copy")}</Span>
+            </Pressable>
+          </View>
+        </DropDownPrimitive.Content>
+      </DropDownPrimitive.Overlay>
+    </DropDownPrimitive.Portal>
+  );
+}
+
 export default function ScannedWord({ dictionaryId, text, lowercase }: Props) {
   const theme = useTheme();
-  const [userData] = useUserDataContext();
-  const [t] = useTranslation();
   const [open, setOpen] = useState(false);
   const triggerRef = useRef<DropDownPrimitive.TriggerRef | null>(null);
 
@@ -24,9 +151,6 @@ export default function ScannedWord({ dictionaryId, text, lowercase }: Props) {
   const definitionMap = useWordDefinitions(dictionaryId, wordDeps);
   const definitionData = definitionMap[lowercase];
   const definitionResult = definitionData && definitionData.definitionsResult;
-  const dictionary = userData.dictionaries.find(
-    (d) => d.id == userData.activeDictionary
-  );
 
   const underlineStyles = [theme.styles.scanWord];
 
@@ -54,114 +178,12 @@ export default function ScannedWord({ dictionaryId, text, lowercase }: Props) {
       </DropDownPrimitive.Trigger>
 
       {open && (
-        <DropDownPrimitive.Portal>
-          <DropDownPrimitive.Overlay
-            style={StyleSheet.absoluteFill}
-            onPress={close}
-          >
-            <DropDownPrimitive.Content align="center">
-              <View
-                style={[
-                  styles.popup,
-                  theme.styles.dialog,
-                  theme.styles.definitionBubble,
-                ]}
-              >
-                {definitionResult && (
-                  <>
-                    <Pressable
-                      style={[styles.bordered, theme.styles.definitionBorders]}
-                      android_ripple={theme.ripples.popup}
-                      onPress={() => {
-                        router.navigate(
-                          `/words/${encodeURIComponent(lowercase)}`
-                        );
-                        close();
-                      }}
-                    >
-                      <Span style={[styles.wordTitle]}>
-                        {definitionResult.spelling}
-                      </Span>
-                    </Pressable>
-
-                    {definitionResult.definitions.map((definition) => {
-                      const partOfSpeech = namePartOfSpeech(
-                        dictionary,
-                        definition.partOfSpeech
-                      );
-
-                      return (
-                        <Pressable
-                          key={definition.id}
-                          style={[
-                            styles.definitionBlock,
-                            styles.bordered,
-                            theme.styles.definitionBorders,
-                          ]}
-                          android_ripple={theme.ripples.popup}
-                          onPress={() => {
-                            router.navigate(
-                              `/words/${encodeURIComponent(
-                                lowercase
-                              )}/definition/${encodeURIComponent(
-                                definition.id
-                              )}`
-                            );
-                            close();
-                          }}
-                        >
-                          <Span style={[theme.styles.partOfSpeech]}>
-                            {partOfSpeech ?? t("unknown")}
-                          </Span>
-
-                          <Span style={[styles.definition]}>
-                            {definition.definition}
-                          </Span>
-
-                          {definition.example.length > 0 && (
-                            <Span
-                              style={[styles.definition, theme.styles.example]}
-                            >
-                              {definition.example}
-                            </Span>
-                          )}
-                        </Pressable>
-                      );
-                    })}
-                  </>
-                )}
-
-                <Pressable
-                  style={[
-                    styles.action,
-                    styles.bordered,
-                    theme.styles.definitionBorders,
-                  ]}
-                  android_ripple={theme.ripples.popup}
-                  onPress={() => {
-                    router.navigate(
-                      `/words/${encodeURIComponent(lowercase)}/definition/add`
-                    );
-                    close();
-                  }}
-                >
-                  <Span>{t("Add_Definition")}</Span>
-                </Pressable>
-
-                <Pressable
-                  style={styles.action}
-                  android_ripple={theme.ripples.popup}
-                  onPress={() => {
-                    Clipboard.setStringAsync(text).catch(logError);
-                    close();
-                  }}
-                >
-                  <Span>{t("Copy")}</Span>
-                </Pressable>
-              </View>
-            </DropDownPrimitive.Content>
-          </DropDownPrimitive.Overlay>
-        </DropDownPrimitive.Portal>
+        <DefinitionsBubble
+          text={text}
+          lowercase={lowercase}
+          definitionResult={definitionResult}
+          close={close}
+        />
       )}
     </DropDownPrimitive.Root>
   );
