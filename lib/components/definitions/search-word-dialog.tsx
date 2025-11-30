bd8a2dbee6405fb2d92d51bef19178f7a930c3cc
Arthur Cosentino <arthurcose@gmail.com>
2025-11-29 20:47:34 -0500

    Implement multiple selection for the search word dialog

--- lib/components/definitions/search-word-dialog.tsx
+++ lib/components/definitions/search-word-dialog.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { listWords, WordDefinitionData } from "@/lib/data";
-import Dialog, { DialogHeader } from "../dialog";
-import { Pressable, StyleSheet, VirtualizedList } from "react-native";
+import Dialog from "../dialog";
+import { Pressable, StyleSheet, View, VirtualizedList } from "react-native";
 import { useUserDataSignal } from "@/lib/contexts/user-data";
 import { useSignalLens } from "@/lib/hooks/use-signal";
 import { logError } from "@/lib/log";
@@ -9,72 +9,18 @@ import { useTranslation } from "react-i18next";
 import { Span } from "../text";
 import { useTheme } from "@/lib/contexts/theme";
 import SearchInput from "../search-input";
-import IconButton from "../icon-button";
-import { ArrowLeftIcon } from "../icons";
 import useWordDefinitions from "@/lib/hooks/use-word-definitions";
-
-function SearchAllWords({
-  words,
-  visible,
-  onSelect,
-}: {
-  words: string[];
-  visible: boolean;
-  onSelect: (spelling: string) => void;
-}) {
-  const theme = useTheme();
-  const [filteredWords, setFilteredWords] = useState<string[]>([]);
-  const [searchValue, setSearchValue] = useState("");
-
-  useEffect(() => {
-    const lowerCaseSearchValue = searchValue.toLowerCase();
-
-    setFilteredWords(
-      words.filter((word) =>
-        word.toLowerCase().startsWith(lowerCaseSearchValue)
-      )
-    );
-  }, [words, searchValue]);
-
-  if (!visible) {
-    return null;
-  }
-
-  return (
-    <>
-      <VirtualizedList
-        data={filteredWords}
-        initialNumToRender={20}
-        keyExtractor={(item: string) => item}
-        getItem={(_, i) => filteredWords[i]}
-        getItemCount={() => filteredWords.length}
-        renderItem={({ item }) => (
-          <Pressable
-            style={styles.row}
-            android_ripple={theme.ripples.transparentButton}
-            onPress={() => onSelect(item)}
-          >
-            <Span>{item}</Span>
-          </Pressable>
-        )}
-      />
-
-      <SearchInput
-        style={styles.searchInput}
-        value={searchValue}
-        onChangeText={setSearchValue}
-      />
-    </>
-  );
-}
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "../icons";
 
 function ListWordDefinitions({
   dictionaryId,
   spelling,
+  selectedDefinitions,
   onSelect,
 }: {
   dictionaryId: number;
   spelling: string;
+  selectedDefinitions: WordDefinitionData[];
   onSelect: (definition: WordDefinitionData) => void;
 }) {
   const [t] = useTranslation();
@@ -91,6 +37,8 @@ function ListWordDefinitions({
     data.dictionaries.find((d) => d.id == dictionaryId)
   )!;
 
+  const buttonStyles = [styles.definitionButton, styles.row];
+
   return (
     <>
       {definitions.map((definition) => {
@@ -98,25 +46,35 @@ function ListWordDefinitions({
           (p) => p.id == definition.partOfSpeech
         )?.name;
 
+        const selected = selectedDefinitions.some((d) => d.id == definition.id);
+
         return (
           <Pressable
             key={definition.id}
-            style={styles.definitionButton}
+            style={buttonStyles}
             android_ripple={theme.ripples.transparentButton}
             onPress={() => onSelect(definition)}
           >
             {/* todo: use DefinitionContent from definition-bubbles instead? mainly wanting a better name for the component */}
-            <Span style={theme.styles.partOfSpeech}>
-              {partOfSpeech ?? t("unknown")}
-            </Span>
-
-            <Span style={styles.definition}>{definition.definition}</Span>
-
-            {definition.example.length > 0 && (
-              <Span style={[styles.definition, theme.styles.example]}>
-                {definition.example}
+            <View>
+              <Span style={theme.styles.partOfSpeech}>
+                {partOfSpeech ?? t("unknown")}
               </Span>
-            )}
+
+              <Span style={styles.definition}>{definition.definition}</Span>
+
+              {definition.example.length > 0 && (
+                <Span style={[styles.definition, theme.styles.example]}>
+                  {definition.example}
+                </Span>
+              )}
+            </View>
+
+            <CheckIcon
+              style={!selected && styles.hidden}
+              size={32}
+              color={theme.colors.iconButton}
+            />
           </Pressable>
         );
       })}
@@ -124,16 +82,29 @@ function ListWordDefinitions({
   );
 }
 
+type Props = { open: boolean; onClose: () => void } & (
+  | {
+      multi?: false;
+      onSelect: (definition: WordDefinitionData) => void;
+    }
+  | {
+      multi: true;
+      onSelect: (definitions: WordDefinitionData[]) => void;
+    }
+);
+
 export default function SearchWordDialog({
   open,
+  multi,
   onSelect,
   onClose,
-}: {
-  open: boolean;
-  onSelect: (definition: WordDefinitionData) => void;
-  onClose: () => void;
-}) {
-  const [selectedWord, setSelectedWord] = useState<string | null>(null);
+}: Props) {
+  const theme = useTheme();
+  const [words, setWords] = useState<string[]>([]);
+  const [filteredWords, setFilteredWords] = useState<string[]>([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [expanded, setExpanded] = useState<{ [key: string]: boolean }>({});
+  const [selectedList, setSelectedList] = useState<WordDefinitionData[]>([]);
 
   const userDataSignal = useUserDataSignal();
   const activeDictionary = useSignalLens(
@@ -141,14 +112,13 @@ export default function SearchWordDialog({
     (data) => data.activeDictionary
   );
 
+  // resets state when opening the dialog
   useEffect(() => {
-    // reset state
     if (open) {
-      setSelectedWord(null);
+      setExpanded({});
+      setSelectedList([]);
     }
   }, [open]);
-
-  const [words, setWords] = useState<string[]>([]);
 
   // possibly causes a lag spike when loading definition editor
   // but prevents visual issues when opening the dialog
@@ -161,41 +131,93 @@ export default function SearchWordDialog({
       .catch(logError);
   }, []);
 
+  useEffect(() => {
+    const lowerCaseSearchValue = searchValue.toLowerCase();
+
+    setFilteredWords(
+      words.filter((word) =>
+        word.toLowerCase().startsWith(lowerCaseSearchValue)
+      )
+    );
+  }, [words, searchValue]);
+
   return (
-    <Dialog
-      open={open}
-      onClose={() => {
-        if (selectedWord != undefined) {
-          setSelectedWord(null);
-        } else {
-          onClose();
-        }
-      }}
-    >
-      {selectedWord && (
-        <>
-          <DialogHeader>
-            <IconButton
-              icon={ArrowLeftIcon}
-              onPress={() => setSelectedWord(null)}
-            />
+    <Dialog open={open} onClose={onClose}>
+      <VirtualizedList
+        data={filteredWords}
+        initialNumToRender={20}
+        keyExtractor={(item: string) => item}
+        getItem={(_, i) => filteredWords[i]}
+        getItemCount={() => filteredWords.length}
+        renderItem={({ item }) => (
+          <View>
+            <Pressable
+              style={styles.row}
+              android_ripple={theme.ripples.transparentButton}
+              onPress={() =>
+                setExpanded({ ...expanded, [item]: !expanded[item] })
+              }
+            >
+              <Span>{item}</Span>
 
-            <Span>{selectedWord}</Span>
-          </DialogHeader>
+              {expanded[item] ? (
+                <ChevronUpIcon size={32} color={theme.colors.iconButton} />
+              ) : (
+                <ChevronDownIcon size={32} color={theme.colors.iconButton} />
+              )}
+            </Pressable>
 
-          <ListWordDefinitions
-            dictionaryId={activeDictionary}
-            spelling={selectedWord}
-            onSelect={(definition) => onSelect(definition)}
-          />
-        </>
-      )}
+            {expanded[item] && (
+              <ListWordDefinitions
+                dictionaryId={activeDictionary}
+                spelling={item}
+                selectedDefinitions={selectedList}
+                onSelect={(definition) => {
+                  if (!multi) {
+                    onSelect(definition);
+                    onClose();
+                    return;
+                  }
 
-      <SearchAllWords
-        visible={selectedWord == undefined}
-        words={words}
-        onSelect={setSelectedWord}
+                  const index = selectedList.findIndex(
+                    (d) => d.id == definition.id
+                  );
+
+                  if (index == -1) {
+                    setSelectedList([...selectedList, definition]);
+                  } else {
+                    setSelectedList(selectedList.toSpliced(index, 1));
+                  }
+                }}
+              />
+            )}
+          </View>
+        )}
       />
+
+      <View style={styles.actions}>
+        <SearchInput
+          style={styles.searchInput}
+          value={searchValue}
+          onChangeText={setSearchValue}
+        />
+
+        {multi && (
+          <Pressable
+            style={[
+              { backgroundColor: theme.colors.primary.default },
+              styles.confirm,
+            ]}
+            android_ripple={theme.ripples.transparentButton}
+            onPress={() => {
+              onSelect(selectedList);
+              onClose();
+            }}
+          >
+            <CheckIcon size={32} color={theme.colors.primary.contrast} />
+          </Pressable>
+        )}
+      </View>
     </Dialog>
   );
 }
@@ -208,16 +230,35 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    height: 48,
+    justifyContent: "space-between",
+    minHeight: 48,
     paddingHorizontal: 16,
+  },
+  actions: {
+    margin: 8,
+    gap: 8,
+    flexDirection: "row",
   },
   searchInput: {
     backgroundColor: "#0004",
-    margin: 8,
+    flex: 1,
+  },
+  confirm: {
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 80,
+    overflow: "hidden",
   },
   definitionButton: {
+    backgroundColor: "#0002",
     paddingVertical: 8,
-    paddingHorizontal: 16,
+    paddingLeft: 32,
+    paddingRight: 16,
     minHeight: 16,
+    gap: 8,
+  },
+  hidden: {
+    opacity: 0,
   },
 });
