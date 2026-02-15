5427acb28eb579bdb6a4d3e2f8a8e87e3ffd3fa9
Arthur Cosentino <arthurcose@gmail.com>
2026-02-15 06:05:22 -0500

    Abstract the relations editor to simplify synonym tests

--- lib/integration-tests/synonyms.ts
+++ lib/integration-tests/synonyms.ts
@@ -1,99 +1,243 @@
-import { assert, assertDeepEq, createWords, LabeledTest } from "./util";
 import {
-  RelationEditorSynonym,
-  RelationsEditorData,
-} from "@/lib/components/definitions/relations-editor";
-import { deleteDictionary, getWordDefinitions } from "@/lib/data";
+  assert,
+  assertDeepEq,
+  createWords,
+  LabeledTest,
+  TestParams,
+} from "./util";
+import { RelationsEditorData } from "@/lib/components/definitions/relations-editor";
+import {
+  deleteDictionary,
+  getWordDefinitions,
+  WordDefinitionData,
+} from "@/lib/data";
 import db from "@/lib/db";
+
+async function getDefinition(dictionaryId: number, lowercaseSpelling: string) {
+  return (await getWordDefinitions(dictionaryId, lowercaseSpelling))!
+    .definitions[0];
+}
+
+async function countClusters() {
+  const result = await db.getFirstAsync<{ "COUNT(*)": number }>(
+    "SELECT COUNT(*) FROM synonym_clusters",
+  );
+  return result!["COUNT(*)"];
+}
+
+class SynonymTestEnvironment {
+  dictionaryId: number;
+  wordDefinitions: { [key: string]: WordDefinitionData | undefined };
+
+  constructor(params: TestParams) {
+    this.wordDefinitions = {};
+    this.dictionaryId = params.nextDictionaryId;
+  }
+
+  async getOrLoadDefinition(lowercaseSpelling: string) {
+    let definition = this.wordDefinitions[lowercaseSpelling];
+
+    if (!definition) {
+      definition = (await getDefinition(this.dictionaryId, lowercaseSpelling))!;
+      this.wordDefinitions[lowercaseSpelling] = definition;
+    }
+
+    return definition;
+  }
+
+  async loadEditor(lowercaseSpelling: string) {
+    const definition = await this.getOrLoadDefinition(lowercaseSpelling);
+    const wrapper = new SynonymEditorWrapper(this, definition);
+
+    await wrapper.reload();
+
+    return wrapper;
+  }
+}
+
+// adapts the relations editor to reduce footguns when writing tests
+class SynonymEditorWrapper {
+  #env: SynonymTestEnvironment;
+  #editor: RelationsEditorData;
+  #spelling: string;
+  #prevConnections: string[];
+
+  constructor(env: SynonymTestEnvironment, definition: WordDefinitionData) {
+    this.#env = env;
+    this.#editor = undefined!; // we'll set this later
+    this.#spelling = definition.spelling;
+    this.#prevConnections = [];
+  }
+
+  async #settle(): Promise<void> {
+    if (this.#editor.totalLoading.get() == 0) {
+      return Promise.resolve();
+    }
+
+    return new Promise((resolve) => {
+      const listener = () => {
+        if (this.#editor.totalLoading.get() == 0) {
+          this.#editor.totalLoading.unsubscribe(listener);
+          resolve();
+        }
+      };
+
+      this.#editor.totalLoading.subscribe(listener);
+    });
+  }
+
+  async reload() {
+    this.#editor = new RelationsEditorData(
+      this.#env.wordDefinitions[this.#spelling],
+    );
+
+    await this.#settle();
+
+    this.#updateConnections();
+  }
+
+  #updateConnections() {
+    this.#prevConnections = this.#editor.synonyms.get().map((w) => w.spelling);
+    this.#prevConnections.push(
+      ...this.#editor.antonyms.get().map((w) => w.spelling),
+    );
+  }
+
+  unlink() {
+    this.#editor.unlink();
+    this.#prevConnections.length = 0;
+  }
+
+  async addSynonyms(lowercaseWords: string[]) {
+    const definitions = await Promise.all(
+      lowercaseWords.map((spelling) => this.#env.getOrLoadDefinition(spelling)),
+    );
+
+    this.#editor.updateWords("Synonyms", [
+      ...this.#editor.synonyms.get(),
+      ...definitions,
+    ]);
+
+    await this.#settle();
+  }
+
+  async setAntonyms(lowercaseWords: string[]) {
+    const definitions = await Promise.all(
+      lowercaseWords.map((spelling) => this.#env.getOrLoadDefinition(spelling)),
+    );
+
+    this.#editor.updateWords("Antonyms", definitions);
+
+    await this.#settle();
+  }
+
+  async setSynonyms(lowercaseWords: string[]) {
+    const definitions = await Promise.all(
+      lowercaseWords.map((spelling) => this.#env.getOrLoadDefinition(spelling)),
+    );
+
+    this.#editor.updateWords("Synonyms", definitions);
+
+    await this.#settle();
+  }
+
+  async addAntonyms(lowercaseWords: string[]) {
+    const definitions = await Promise.all(
+      lowercaseWords.map((spelling) => this.#env.getOrLoadDefinition(spelling)),
+    );
+
+    this.#editor.updateWords("Antonyms", [
+      ...this.#editor.antonyms.get(),
+      ...definitions,
+    ]);
+
+    await this.#settle();
+  }
+
+  async save() {
+    const definition = this.#env.wordDefinitions[this.#spelling]!;
+    await this.#editor.save(definition);
+
+    const synonymsId = this.#editor.synonymsId.get();
+    const antonymsId = this.#editor.antonymsId;
+
+    definition.synonymsId = synonymsId;
+
+    // unset synonymIds for old connections
+    for (const spelling of this.#prevConnections) {
+      const definition = this.#env.wordDefinitions[spelling];
+
+      if (definition) {
+        definition.synonymsId = null;
+      }
+    }
+
+    // set synonymIds for the latest synonyms
+    for (const { spelling } of this.#editor.synonyms.get()) {
+      const definition = this.#env.wordDefinitions[spelling];
+
+      if (definition) {
+        definition.synonymsId = synonymsId;
+      }
+    }
+
+    // set synonymIds for the latest antonyms
+    for (const { spelling } of this.#editor.antonyms.get()) {
+      const definition = this.#env.wordDefinitions[spelling];
+
+      if (definition) {
+        definition.synonymsId = antonymsId;
+      }
+    }
+
+    // update connections for future changes
+    this.#updateConnections();
+  }
+
+  assertSynonyms(expected: string[], message: string) {
+    assertDeepEq(
+      this.#editor.synonyms.get().map((w) => w.spelling),
+      expected,
+      message,
+    );
+  }
+
+  assertAntonyms(expected: string[], message: string) {
+    assertDeepEq(
+      this.#editor.antonyms.get().map((w) => w.spelling),
+      expected,
+      message,
+    );
+  }
+}
 
 export const SYNONYM_TESTS: LabeledTest[] = [
   [
     "Synonyms",
     async (params) => {
-      async function countClusters() {
-        const result = await db.getFirstAsync<{ "COUNT(*)": number }>(
-          "SELECT COUNT(*) FROM synonym_clusters",
-        );
-        return result!["COUNT(*)"];
-      }
-
       const originalClusterCount = await countClusters();
 
       const dictionaryId = params.nextDictionaryId;
 
       await createWords(dictionaryId, ["a", "b", "c", "d"]);
 
-      async function getDefinition(lowercaseSpelling: string) {
-        return (await getWordDefinitions(dictionaryId, lowercaseSpelling))!
-          .definitions[0];
-      }
-
-      function assertRelationWords(
-        words: RelationEditorSynonym[],
-        expected: string[],
-        message: string,
-      ) {
-        assertDeepEq(
-          words.map((w) => w.spelling),
-          expected,
-          message,
-        );
-      }
-
-      function settle(data: RelationsEditorData): Promise<void> {
-        if (data.totalLoading.get() == 0) {
-          return Promise.resolve();
-        }
-
-        return new Promise((resolve) => {
-          const listener = () => {
-            if (data.totalLoading.get() == 0) {
-              data.totalLoading.unsubscribe(listener);
-              resolve();
-            }
-          };
-
-          data.totalLoading.subscribe(listener);
-        });
-      }
-
-      let wordA = await getDefinition("a");
-      let wordB = await getDefinition("b");
-      let wordC = await getDefinition("c");
+      const env = new SynonymTestEnvironment(params);
 
       // set relation between 'a' and 'b'
-      let relationsA = new RelationsEditorData(wordA);
-      await settle(relationsA);
+      const relationsA = await env.loadEditor("a");
+      await relationsA.addSynonyms(["b", "c"]);
+      relationsA.assertSynonyms(["b", "c"], "Added synonyms");
 
-      relationsA.updateWords("Synonyms", [wordB, wordC]);
-      await settle(relationsA);
-      assertRelationWords(
-        relationsA.synonyms.get(),
-        ["b", "c"],
-        "Added synonyms",
-      );
+      await relationsA.addAntonyms(["c"]);
+      relationsA.assertSynonyms(["b"], "Removed 'c' from synonyms");
+      relationsA.assertAntonyms(["c"], "Moved 'c' to antonyms");
 
-      relationsA.updateWords("Antonyms", [wordC]);
-      await settle(relationsA);
-      assertRelationWords(
-        relationsA.synonyms.get(),
-        ["b"],
-        "Removed 'c' from synonyms",
-      );
-      assertRelationWords(
-        relationsA.antonyms.get(),
-        ["c"],
-        "Moved 'c' to antonyms",
-      );
-
-      await relationsA.save(wordA);
+      await relationsA.save();
 
       // verify everything saved
-      wordA = await getDefinition("a");
-      relationsA = new RelationsEditorData(wordA);
-      await settle(relationsA);
-      assertRelationWords(relationsA.synonyms.get(), ["b"], "Synonyms saved");
-      assertRelationWords(relationsA.antonyms.get(), ["c"], "Antonyms saved");
+      await relationsA.reload();
+      relationsA.assertSynonyms(["b"], "Synonyms saved");
+      relationsA.assertAntonyms(["c"], "Antonyms saved");
       assertDeepEq(
         originalClusterCount + 2,
         await countClusters(),
@@ -101,9 +245,9 @@ export const SYNONYM_TESTS: LabeledTest[] = [
       );
 
       // reload words
-      wordA = await getDefinition("a");
-      wordB = await getDefinition("b");
-      wordC = await getDefinition("c");
+      const wordA = await getDefinition(dictionaryId, "a");
+      const wordB = await getDefinition(dictionaryId, "b");
+      const wordC = await getDefinition(dictionaryId, "c");
 
       assert(wordA.synonymsId != null, "Synonyms set");
       assertDeepEq(
@@ -117,64 +261,37 @@ export const SYNONYM_TESTS: LabeledTest[] = [
       );
 
       // loading existing synonyms into synonyms
-      let wordD = await getDefinition("d");
-      let relationsD = new RelationsEditorData(wordD);
-      await settle(relationsD);
-
-      relationsD.updateWords("Synonyms", [wordB]);
-      await settle(relationsD);
-      assertRelationWords(
-        relationsD.synonyms.get(),
+      const relationsD = await env.loadEditor("d");
+      await relationsD.addSynonyms(["b"]);
+      relationsD.assertSynonyms(
         ["a", "b"],
         "Loaded 'a' from 'b's synonyms into synonyms",
       );
-      assertRelationWords(
-        relationsD.antonyms.get(),
+      relationsD.assertAntonyms(
         ["c"],
         "Loaded 'c' from 'b's antonyms into antonyms",
       );
 
-      relationsD.updateWords("Antonyms", [...relationsD.antonyms.get(), wordB]);
-      await settle(relationsD);
-      assertRelationWords(
-        relationsD.synonyms.get(),
-        ["a"],
-        "Removed 'b' from synonyms",
-      );
-      assertRelationWords(
-        relationsD.antonyms.get(),
-        ["b", "c"],
-        "Moved 'b' to antonyms",
-      );
+      await relationsD.addAntonyms(["b"]);
+      relationsD.assertSynonyms(["a"], "Removed 'b' from synonyms");
+      relationsD.assertAntonyms(["b", "c"], "Moved 'b' to antonyms");
 
       // loading existing synonyms into antonyms
-      relationsD = new RelationsEditorData(wordD);
-      await settle(relationsD);
-
-      relationsD.updateWords("Antonyms", [wordB]);
-      await settle(relationsD);
-
-      assertRelationWords(
-        relationsD.synonyms.get(),
+      await relationsD.reload();
+      await relationsD.addAntonyms(["b"]);
+      relationsD.assertSynonyms(
         ["c"],
         "Loaded 'c' from 'b's antonyms into synonyms",
       );
-      assertRelationWords(
-        relationsD.antonyms.get(),
+      relationsD.assertAntonyms(
         ["a", "b"],
         "Loaded 'a' from 'b's synonyms into antonyms",
       );
-      await relationsD.save(wordD);
+      await relationsD.save();
 
       // verify save
-      wordA = await getDefinition("a");
-      relationsA = new RelationsEditorData(wordA);
-      await settle(relationsA);
-      assertRelationWords(
-        relationsA.antonyms.get(),
-        ["c", "d"],
-        "'d' exists in 'a's antonyms",
-      );
+      await relationsA.reload();
+      relationsA.assertAntonyms(["c", "d"], "'d' exists in 'a's antonyms");
       assertDeepEq(
         originalClusterCount + 2,
         await countClusters(),
@@ -182,28 +299,21 @@ export const SYNONYM_TESTS: LabeledTest[] = [
       );
 
       // unlink
-      wordD = await getDefinition("d");
       relationsD.unlink();
-      await relationsD.save(wordD);
+      await relationsD.save();
 
-      wordA = await getDefinition("a");
-      relationsA = new RelationsEditorData(wordA);
-      await settle(relationsA);
-      assertRelationWords(
-        relationsA.antonyms.get(),
+      await relationsA.reload();
+      relationsA.assertAntonyms(
         ["c"],
         "'d' removed from 'a's antonyms after unlinking",
       );
 
       // deleting synonym group by emptying antonyms
-      relationsA.updateWords("Antonyms", []);
-      await settle(relationsA);
-      await relationsA.save(wordA);
+      await relationsA.setAntonyms([]);
+      await relationsA.save();
 
-      wordA = await getDefinition("a");
-      relationsA = new RelationsEditorData(wordA);
-      await settle(relationsA);
-      assertRelationWords(relationsA.antonyms.get(), [], "No antonyms for 'a'");
+      await relationsA.reload();
+      relationsA.assertAntonyms([], "No antonyms for 'a'");
       assertDeepEq(
         originalClusterCount + 1,
         await countClusters(),
@@ -211,10 +321,8 @@ export const SYNONYM_TESTS: LabeledTest[] = [
       );
 
       // Add 'd' back to antonyms to prep for testing unlinking
-      wordD = await getDefinition("d");
-      relationsA.updateWords("Antonyms", [wordD]);
-      await settle(relationsA);
-      await relationsA.save(wordA);
+      await relationsA.setAntonyms(["d"]);
+      await relationsA.save();
 
       assertDeepEq(
         originalClusterCount + 2,
@@ -223,13 +331,11 @@ export const SYNONYM_TESTS: LabeledTest[] = [
       );
 
       // unlink
-      wordD = await getDefinition("d");
-      relationsD = new RelationsEditorData(wordD);
-      await settle(relationsD);
+      await relationsD.reload();
       relationsD.unlink();
-      await relationsD.save(wordD);
+      await relationsD.save();
 
-      wordD = await getDefinition("d");
+      const wordD = await getDefinition(dictionaryId, "d");
       assertDeepEq(
         wordD.synonymsId,
         null,
